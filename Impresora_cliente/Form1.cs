5fcd5dfd57fda10e040a849d239b3b4ab78b9a90
Mario Vila Barros <mako-32@hotmail.com>
2018-08-19 21:21:44 +0200

    19-8-2018

--- Impresora_cliente/Form1.cs
+++ Impresora_cliente/Form1.cs
@@ -3,18 +3,62 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Impresora_cliente
 {
     public partial class Form1 : Form
     {
+        static string ip = "127.0.0.1";
+        static int puerto = 31416;
+        IPEndPoint ie;
+        Socket servidor;
+        NetworkStream ns;
+        StreamReader sr;
+        StreamWriter sw;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void conexion(string ip, int puerto)
+        {
+            try
+            {
+                ie = new IPEndPoint(IPAddress.Parse(ip), puerto);
+                servidor = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                servidor.Connect(ie);
+                ns = new NetworkStream(servidor);
+                sr = new StreamReader(ns);
+                sw = new StreamWriter(ns);
+
+                if (servidor.Available == 0)
+                {
+                    label1.Text += "Enviado";
+                }
+                servidor.Close();
+            }
+            catch (SocketException ex)
+            {
+                label1.Text = String.Format("Error de conexión: {0}" + Environment.NewLine + "Código de error: {1}({2})", ex.Message, (SocketError)ex.ErrorCode, ex.ErrorCode);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            conexion(ip, puerto);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //label1.Text = "Conectado a: ";
         }
     }
 }
---
+++ Impresora_servidor/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace Impresora_servidor
+{
+    class Program
+    {
+        string carpeta = Path.GetTempPath();
+        static bool conectar = false, apagar = false;
+        Socket s = null;
+
+        public void iniciaServidorImpresora()
+        {
+            try
+            {
+                IPEndPoint ie = new IPEndPoint(IPAddress.Any, 31416);
+                s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                s.Bind(ie);
+                s.Listen(5);
+                Console.WriteLine("Usando puerto: " + 31416);
+                Console.WriteLine("A la espera");
+                conectar = true;
+
+                while (conectar)
+                {
+                    Socket cliente = s.Accept();
+                    Thread hilo = new Thread(hiloCliente);
+                    hilo.IsBackground = true;
+                    hilo.Start(cliente);
+                }
+            }
+            catch (SocketException)
+            {
+                conectar = false;
+                if (!apagar)
+                    Console.WriteLine("Puerto " + 31416 + " ocupado. Cerrando servidor.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+            }
+        }
+
+        public void hiloCliente(object socket)
+        {
+            //TODO comprobar servidor tiene impresora 
+            Socket cliente = (Socket)socket;
+            IPEndPoint ieCliente = (IPEndPoint)cliente.RemoteEndPoint;
+            Console.WriteLine("Conectado con el cliente {0} en el puerto {1}", ieCliente.Address, ieCliente.Port);
+            NetworkStream ns = new NetworkStream(cliente);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            string mensaje;
+            try
+            {
+                mensaje = sr.ReadLine();
+                Console.WriteLine(mensaje);
+              
+            }
+            catch (IOException)
+            {
+
+            }
+            catch (ObjectDisposedException)
+            {
+
+            }
+            catch (SocketException)
+            {
+                cliente.Close();
+                s.Close();
+            }
+            finally
+            {
+                sw.Close();
+                sr.Close();
+                ns.Close();
+
+                cliente.Close();
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program imp = new Program();
+            imp.iniciaServidorImpresora();
+            Console.ReadLine();
+
+        }
+    }
+}
